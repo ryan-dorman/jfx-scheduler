cb159903073006cb52546fb7562e3170ad8be27e
Ryan Dorman <ryandormanthe1@gmail.com>
2021-01-29 23:50:54 -0600

    testing

--- src/main/java/info/ryandorman/simplescheduler/Main.java
+++ src/main/java/info/ryandorman/simplescheduler/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Simple Scheduler");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
