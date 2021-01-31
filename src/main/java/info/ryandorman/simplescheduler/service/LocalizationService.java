528538a4614c6e5ea5eba7d40208be3804336166
Ryan Dorman <ryandormanthe1@gmail.com>
2021-01-31 11:41:42 -0600

    restructure

---
+++ src/main/java/info/ryandorman/simplescheduler/service/LocalizationService.java
@@ -0,0 +1,5 @@
+package info.ryandorman.simplescheduler.service;
+
+public class LocalizationService {
+    // Handle all time and language localization
+}
---
+++ src/main/java/info/ryandorman/simplescheduler/service/SchedulingService.java
@@ -0,0 +1,7 @@
+package info.ryandorman.simplescheduler.service;
+
+public class SchedulingService {
+    // scheduleAppointment
+    // ensuring appointments have valid time windows based on customers schedules
+    // is business login that doesn't belong in a dao or controller
+}
