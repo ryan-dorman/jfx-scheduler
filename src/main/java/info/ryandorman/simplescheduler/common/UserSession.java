d25283e79e268e0091ee9e0aca30489c7f3a5229
Dormanator <ryandormanthe1@gmail.com>
2021-01-31 22:41:31 -0600

    JavaDoc updated

--- src/main/java/info/ryandorman/simplescheduler/common/UserSession.java
+++ src/main/java/info/ryandorman/simplescheduler/common/UserSession.java
@@ -1,17 +1,30 @@
 package info.ryandorman.simplescheduler.common;
 
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
+/**
+ * A singleton object used for storing the current user's session in the scheduling application. It stores information
+ * useful for tracking the current user such as their username and the location and time their session started from.
+ */
 public class UserSession {
     private static UserSession session;
     private long userId;
     private String userName;
     private ZoneId userZone;
-    private boolean loggedIn = false;
+    private ZonedDateTime sessionStart;
 
+    /**
+     * Private constructor cannot be called externally. This singleton requires Session initialization via
+     * <code>getInstance()</code>.
+     */
     private UserSession() {
     }
 
+    /**
+     *
+     * @return
+     */
     public static UserSession getInstance () {
         if (session == null) {
             return new UserSession();
@@ -44,11 +57,11 @@ public class UserSession {
         this.userZone = userZone;
     }
 
-    public boolean isLoggedIn() {
-        return loggedIn;
+    public ZonedDateTime getSessionStart() {
+        return sessionStart;
     }
 
-    public void setLoggedIn(boolean loggedIn) {
-        this.loggedIn = loggedIn;
+    public void setSessionStart(ZonedDateTime sessionStart) {
+        this.sessionStart = sessionStart;
     }
 }
