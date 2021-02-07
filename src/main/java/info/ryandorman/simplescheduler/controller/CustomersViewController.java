e3497c96a3229c78b1aaeb6767b205a26d4e6d7e
dormanator <ryandormanthe1@gmail.com>
2021-02-06 23:15:31 -0600

    customer tableview started

--- src/main/java/info/ryandorman/simplescheduler/controller/CustomersViewController.java
+++ src/main/java/info/ryandorman/simplescheduler/controller/CustomersViewController.java
@@ -1,13 +1,43 @@
 package info.ryandorman.simplescheduler.controller;
 
+import info.ryandorman.simplescheduler.model.Customer;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CustomersViewController implements Initializable {
+
+    // Customers Table
+    @FXML
+    private TableView<Customer> customersTable;
+    @FXML
+    private TableColumn<Customer, Integer> idColumn;
+    @FXML
+    private TableColumn<Customer, String> nameColumn;
+    @FXML
+    private TableColumn<Customer, String> phoneColumn;
+    @FXML
+    private TableColumn<Customer, String> addressColumn;
+    @FXML
+    private TableColumn<Customer, String> postalCodeColumn;
+    @FXML
+    private TableColumn<Customer, String> divisionColumn;
+
+    //
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Setup Customers Table View Columns
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+        postalCodeColumn.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
+        postalCodeColumn.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
 
     }
 }
